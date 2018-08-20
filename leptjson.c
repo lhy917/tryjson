b7210389feef035f6ad1bfe5117ab8220f634b1e
lihongyi <lhyi2013@hotmail.com>
2018-08-21 00:03:54 +0800

    重构了true、false、null的解析函数

--- leptjson.c
+++ leptjson.c
@@ -42,6 +42,17 @@ static int lept_parse_null(lept_context *c, lept_value *v) {
     return LEPT_PARSE_OK;
 }
 
+static int lept_parse_literal(lept_context *c, lept_value *v, const char *literal, lept_type type) {
+    size_t i;
+    EXPECT(c,  literal[0]);
+    for (i = 0; literal[i + 1]; i++)
+        if (c->json[i] != literal[i + 1])
+            return LEPT_PARSE_INVALID_VALUE;
+    c->json += i;
+    v->type = type;
+    return LEPT_PARSE_OK;
+}
+
 static int lept_parse_number(lept_context *c, lept_value *v) {
     char *end;
 
@@ -55,9 +66,12 @@ static int lept_parse_number(lept_context *c, lept_value *v) {
 
 static int lept_parse_value(lept_context *c, lept_value *v) {
     switch(*c->json) {
-        case 't': return lept_parse_true(c, v);
-        case 'f': return lept_parse_false(c, v);
-        case 'n': return lept_parse_null(c, v);
+        // case 't': return lept_parse_true(c, v);
+        // case 'f': return lept_parse_false(c, v);
+        // case 'n': return lept_parse_null(c, v);
+        case 't': return lept_parse_literal(c, v, "true", LEPT_TRUE);
+        case 'f': return lept_parse_literal(c, v, "false", LEPT_FALSE);
+        case 'n': return lept_parse_literal(c, v, "null", LEPT_NULL);
         default: return lept_parse_number(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
     }
